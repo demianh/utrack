80734df46e288080d4f9a5a50a6f709458a14e46
Demian Holderegger <demian.holderegger@gmail.com>
2015-04-23 11:39:27 +0200

    adding workflow queries

--- queries/queries.js
+++ queries/queries.js
@@ -23,10 +23,11 @@ MongoClient.connect('mongodb://127.0.0.1:27017/wtrack', function(err, connection
 
 	// ================== Queries ==================
 
+/*
 	// --- find by Naviagtion Item
 	//db.getCollection('log').find({ 'workflow.mainnav': 'Administration'}, out);
 
-/*
+
 	// --- count event types
 	db.getCollection('log').aggregate([
 		{ $group: {
@@ -55,7 +56,7 @@ MongoClient.connect('mongodb://127.0.0.1:27017/wtrack', function(err, connection
 	], out);
 */
 
-
+/*
 	// --- session duration
 	db.getCollection('log').find({ $or: [{'event': 'session_start'}, {'event': 'session_end'}]}).sort({ timestamp: 1 }).toArray(function(err, data){
 
@@ -90,8 +91,9 @@ MongoClient.connect('mongodb://127.0.0.1:27017/wtrack', function(err, connection
 		console.log('Average Time: ' + Math.round(total_time/times.length) + ' sec');
 		console.log('Samples: ' + times.length);
 	});
+*/
 
-
+/*
 	// --- section duration
 	db.getCollection('log').find({'workflow.mainnav': { $ne: null}}).sort({ timestamp: 1 }).toArray(function(err, data){
 
@@ -131,6 +133,76 @@ MongoClient.connect('mongodb://127.0.0.1:27017/wtrack', function(err, connection
 
 		console.log(times);
 	});
+*/
+
+/*
+	// --- get list of workflows
+	db.getCollection('log').aggregate([
+		{ $match: { 'workflow.dialog.0': {$exists: true}}},
+		{ $group: {
+			_id: '$workflow', count: { $sum: 1}
+		}},
+		{ $sort : { count : -1}}
+	], function(err, data){
+		console.log(data);
+	});
+*/
+
+	// --- get time used in workflows
+	db.getCollection('log').aggregate([
+		{ $match: {
+			$or: [{ event: "dialog_open" }, { event: "dialog_close" }]
+		}},
+		{ $project: {
+			'workflow': '$workflow',
+			'event': '$event',
+			'session': '$session',
+			'timestamp': '$timestamp'
+		}},
+		{ $sort : { timestamp : 1}}
+	], function(err, data){
+		console.log(data);
+
+
+		var sessions = {};
+		var times = {};
+		data.forEach(function(row) {
+			if(row.session){
+				var section = [row.workflow.mainnav, row.workflow.subnav, row.workflow.dialog.join(' / ')].join(' / ');
+				if(sessions[row.session.id]){
+					// update session if section changed or session ended
+					var old_section = sessions[row.session.id].section;
+					if (old_section != section || row.event == 'session_end'){
+						if(!times[old_section]){
+							times[old_section] = row.timestamp - sessions[row.session.id].timestamp;
+						} else {
+							times[old_section] += row.timestamp - sessions[row.session.id].timestamp
+						}
+
+						sessions[row.session.id] = {
+							section: section,
+							timestamp: row.timestamp
+						};
+					}
+				} else {
+					// start session
+					sessions[row.session.id] = {
+						section: section,
+						timestamp: row.timestamp
+					};
+				}
+			}
+		});
+
+		// convert to seconds
+		Object.keys(times).map(function(value, index) {
+			times[value] = Math.round(times[value]/1000) + ' sec';
+		});
+
+		console.log(times);
+
+	});
+
 
 });
 
