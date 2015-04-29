a463340b3db2043f02e7a0b3caefee75aae3ff4d
Demian Holderegger <demian.holderegger@gmail.com>
2015-04-29 13:08:59 +0200

    Restart Server if needed

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# Start or restart
+forever stop src/backend/server.js
 forever start src/backend/server.js
--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Fetch source
 git pull origin master
 
+# Install Packages
 npm install
+
+# Restart Server
+./start_server.sh
